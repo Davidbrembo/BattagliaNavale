78bc48137f3bac14c6e9840aa11809cbc634c32a
Davidbrembo <davidbremba@gmail.com>
2025-04-23 00:16:04 +0200

    Creazione schermata iniziale
    Suoni e immagine

--- BattagliaNavale/src/app/Applicazione.java
+++ BattagliaNavale/src/app/Applicazione.java
@@ -1,22 +1,66 @@
 package app;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import java.io.File;
 
 public class Applicazione extends Application {
 
+    private MediaPlayer mediaPlayer;
+
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("JavaFX funziona!");
-        StackPane root = new StackPane(label);
-        Scene scene = new Scene(root, 300, 200);
-        
+        // Ottieni dimensioni dello schermo
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        double screenWidth = screenBounds.getWidth();
+        double screenHeight = screenBounds.getHeight();
+
+        // Immagine di sfondo
+        Image image = new Image("file:resources/battaglia.jpg");
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(false); // Disabilita il mantenimento delle proporzioni
+        imageView.setFitWidth(screenWidth); // Imposta la larghezza dell'immagine a tutta la finestra
+        imageView.setFitHeight(screenHeight); // Imposta l'altezza dell'immagine a tutta la finestra
+
+        // Usando AnchorPane per evitare spazi extra
+        AnchorPane root = new AnchorPane();
+        root.getChildren().add(imageView);
+
+        // Fissiamo i limiti dell'immagine alla finestra
+        AnchorPane.setTopAnchor(imageView, 0.0);
+        AnchorPane.setRightAnchor(imageView, 0.0);
+        AnchorPane.setBottomAnchor(imageView, 0.0);
+        AnchorPane.setLeftAnchor(imageView, 0.0);
+
+        // Crea la scena
+        Scene scene = new Scene(root, screenWidth, screenHeight);
+
+        // Audio
+        Media sound = new Media(new File("resources/audio_battaglia.mp3").toURI().toString());
+        mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+
+        // Setup finestra
         primaryStage.setTitle("Battaglia Navale");
         primaryStage.setScene(scene);
+        primaryStage.setFullScreen(true); // Forza la modalità a schermo intero
         primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
     }
 
     public static void main(String[] args) {
