c4b291848ba02ba9900b2853f8bef797e977e0c7
Davidbrembo <davidbremba@gmail.com>
2025-04-22 14:43:05 +0200

    Aggiunto JavaFX

--- BattagliaNavale/src/app/Applicazione.java
+++ BattagliaNavale/src/app/Applicazione.java
@@ -1,10 +1,25 @@
 package app;
 
-public class Applicazione {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		System.out.println("Battaglia navale");
+public class Applicazione extends Application {
 
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("JavaFX funziona!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 200);
+        
+        primaryStage.setTitle("Battaglia Navale");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
